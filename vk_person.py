4d52a983b9e5645f73e6f4acaa6ba7268042f1e2
Мызников Евгений <junaers1996@gmail.com>
2022-05-08 12:20:02 +0300

    fixed bug in user_search.id

--- vk_person.py
+++ vk_person.py
@@ -11,9 +11,10 @@ def user_search():
     info = user_information()
     person_base = []
     data = vk.method("users.search", {f"q":" ","sort":"0","offset":"0","count":"100", "city":{info[2]}, "sex":{info[0]}, "status":"1", "age_from":{info[1]-3}, "age_to":{info[1]+3}})
-    id = data['items'][0]['id']
+
 
     for i in range(len(data['items'])):
+        id = data['items'][i]['id']
         search_request = [f'https://vk.com/id{id}', data['items'][i]['first_name'], data['items'][i]['last_name']]
         person_base.append(search_request)
     # print(data['items'][0]['first_name'])
