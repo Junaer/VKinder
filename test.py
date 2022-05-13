ab1f269a03daa5549ea34fc52f1ae30d2c8a10c3
Мызников Евгений <junaers1996@gmail.com>
2022-05-13 18:41:25 +0300

    pass1

--- test.py
+++ test.py
@@ -6,12 +6,3 @@ from random import randrange
 f = open('token.txt', 'r')
 token_vk_group = f.readline()
 token_vk_person = f.readline()
-
-token_person = token_vk_person
-vk_person = vk_api.VkApi(token=token_person)
-
-def user_information():
-    info = vk_person.method("status.get", {'user_id': 87343268})
-    print(info)
-
-user_information()
